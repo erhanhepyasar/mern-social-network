525b036f2bd1f2c4f001c40786d397c2f1d156b4
Erhan Hepyaşar <200189@gm.isbank.com.tr>
2021-01-02 21:39:23 +0300

    added

---
+++ README.md
@@ -0,0 +1,4 @@
+
+https://udemy.com/course/mern-stack-front-to-back/learn/lecture/14555594#overview
+
+40
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
 import React, { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { setAlert } from '../../actions/alert'
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -11,12 +16,20 @@ const Register = () => {
     const { name, email, password, password2 } = formData;
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+    const onSubmit = async e => {
+        e.preventDefault();
+        if(password !== password2) {
+            setAlert('Passwords do not match', 'danger')
+        } else {
+            console.log('SUCCESS')
+        }
+    }
 
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-            <form className="form" action="create-profile.html">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                 <input 
                     type="text" 
@@ -62,10 +75,17 @@ const Register = () => {
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     )
+};
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
 }
 
-export default Register
+export default connect(
+    null, 
+    { setAlert })
+    (Register);
